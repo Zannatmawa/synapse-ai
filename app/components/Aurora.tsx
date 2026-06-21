350b3c63dc127d6ce3814f0c11c68d325fdfd025
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-21 19:46:21 +0600

    feat: add GridBackground and animated Aurora background components

--- app/components/Aurora.tsx
+++ app/components/Aurora.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 const Aurora = () => {
     useGSAP(() => {
@@ -22,13 +24,22 @@ const Aurora = () => {
             yoyo: true,
             ease: "sine.inOut",
         });
+        gsap.to(".aurora-1", {
+            x: 150,
+            y: 80,
+            rotation: 20,
+            duration: 12,
+            repeat: -1,
+            yoyo: true,
+            ease: "sine.inOut",
+        });
     });
 
     return (
         <div className="absolute inset-0 overflow-hidden pointer-events-none">
-            <div className="aurora-1 absolute w-[500px] h-[500px] rounded-full bg-primary/20 blur-[150px]" />
+            <div className="aurora-1 absolute w-[500px] h-[500px] rounded-full bg-[#00ffb3]/15 blur-[150px]" />
 
-            <div className="aurora-2 absolute right-0 top-20 w-[400px] h-[400px] rounded-full bg-cyan-400/10 blur-[150px]" />
+            <div className="aurora-2 absolute right-0 top-20 w-[400px] h-[400px] rounded-full bg-cyan-500/10 blur-[150px]" />
         </div>
     );
 };
